7ed67f606091d3f0035affc38342972eb3dc7432
Kishiannn_ <abudachan122@gmail.com>
2025-02-21 23:32:39 +0800

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,9 +1,13 @@
 package main;
 
 public class Game {
+    
     private GameWindow gameWindow;
+    private GamePanel gamePanel;
+    
     public Game() {
-        gameWindow = new GameWindow();
+        gamePanel = new GamePanel();
+        gameWindow = new GameWindow(gamePanel);
     }
 }
 
